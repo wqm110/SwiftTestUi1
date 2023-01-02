9786e3566468aee1d0e600b963b0847174013bb2
wqm <1003053968@qq.com>
2023-01-02 13:31:13 +0800

    Initial Commit

---
+++ SwiftTestUi1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftTestUi1
+//
+//  Created by 王庆敏 on 2023/1/2.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftTestUi1/SwiftTestUi1App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftTestUi1App.swift
+//  SwiftTestUi1
+//
+//  Created by 王庆敏 on 2023/1/2.
+//
+
+import SwiftUI
+
+@main
+struct SwiftTestUi1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
